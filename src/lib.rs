daff607014e7db38af9a4f50e757f9504ec6eee5
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2020-11-30 21:18:57 -0800

    Added answer checking

--- src/lib.rs
+++ src/lib.rs
@@ -23,11 +23,31 @@ macro_rules! aoc {
             (|input| Box::new([<day $day>]::solutions(input, Part::Part1)), |input| Box::new([<day $day>]::solutions(input, Part::Part2)))
         }
     };
+    ($day: expr, $ans1: expr) => {
+        paste::item! {
+            (|input| {
+                let ans =[<day $day>]::part1(input);
+                assert_eq!(ans, $ans1);
+                Box::new(ans)
+            }, |input| Box::new([<day $day>]::part2(input)))
+        }
+    };
+    ($day: expr, $ans1: expr, $ans2: expr) => {
+            (|input| {
+                paste::item! { let ans = [<day $day>]::part1(input); }
+                assert_eq!(ans, $ans1);
+                Box::new(ans)
+            }, |input| {
+                paste::item! { let ans =[<day $day>]::part2(input); }
+                assert_eq!(ans, $ans2);
+                Box::new(ans)
+            })
+    };
 }
 
 pub fn get_day(day: u32) -> (DayFn, DayFn) {
     return match day {
-        1 => aoc!(01),
+        1 => aoc!(01, 539851, 212481360),
         _ => {
             eprintln!("Unknown day: {}", day);
             return (noop, noop);
