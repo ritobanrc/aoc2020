75997297e3ad8a0f55b58ccfd630b1b3c17f0269
Ritoban Roy-Chowdhury <ritobanrc@gmail.com>
2020-12-02 09:10:59 -0800

    Parser combinator for day 2, and modified `get` to source `.cookie`

--- src/day02.rs
+++ src/day02.rs
@@ -27,8 +27,40 @@ fn parse(input: &str) -> Result<Vec<(usize, usize, char, &str)>> {
         .collect::<Result<Vec<_>>>()?)
 }
 
+// Parser combinator approach
+fn parse2(input: &str) -> Vec<(usize, usize, char, String)> {
+    use combine::{
+        many1,
+        parser::char::{digit, letter, space},
+        token, Parser,
+    };
+    input
+        .lines()
+        .map(|line| {
+            (
+                many1::<String, _, _>(digit()),
+                token('-'),
+                many1::<String, _, _>(digit()),
+                space(),
+                letter(),
+                token(':'),
+                space(),
+                many1::<String, _, _>(letter()),
+            )
+                .map(|(min, _, max, _, c, _, _, passwd)| {
+                    let min: usize = str::parse(&min).unwrap();
+                    let max: usize = str::parse(&max).unwrap();
+                    (min, max, c, passwd)
+                })
+                .parse(line)
+                .unwrap()
+                .0
+        })
+        .collect()
+}
+
 pub fn part1(input: String) -> usize {
-    let input = parse(&input).expect("Day 02 Part 1 -- Failed to Parse");
+    let input = parse2(&input); //.expect("Day 02 Part 1 -- Failed to Parse");
     input
         .into_iter()
         .filter(|(min, max, c, passwd)| {
